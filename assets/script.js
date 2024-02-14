c60f78b028559e6448c3d97a3a0abcee723f1f38
Brice123456zz <Your Emailbrice.seguret@gmail.com>
2024-02-14 18:25:07 +0100

    slide javascript

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,46 @@
-const slides = [
+const slides = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png"]
+let numero = 2;
+function ChangeSlide(sense) {
+	numero = numero + sense;
+	if (numero > 3)
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		numero = 0;
 	}
-]
+	if (numero < 0)
+	{
+		numero = 3;
+	}
+	document.getElementById("banner1").src = "./assets/images/slideshow/" + slides[numero];
 
-function previousSlide() {
-	document.querySelector('.banner-img').scrollLeft += '20';
+	
 }
+const dots = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png"]
+
+
+function Changedots(numero2) {
+	document.getElementById("banner1").src = "./assets/images/slideshow/" + dots[numero2];
+}
+
+
+function ouvrirPage () {
+	var a = document.getElementById("#search").value;
+}
+
+// const slides = [
+// 	{
+// 		"image":"slide1.jpg",
+// 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+// 	},
+// 	{
+// 		"image":"slide2.jpg",
+// 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+// 	},
+// 	{
+// 		"image":"slide3.jpg",
+// 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+// 	},
+// 	{
+// 		"image":"slide4.png",
+// 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+// 	}
+// ]
