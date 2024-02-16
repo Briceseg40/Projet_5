2cc74c784c8dbc53a338068b6ebd85d4d330f810
Brice123456zz <Your Emailbrice.seguret@gmail.com>
2024-02-16 17:16:04 +0100

    slide terminé

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
-const slides = ["slide1.jpg", "slide2.jpg", "slide4.png","slide3.jpg"]
-let numero = 2;
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	}
+]
+
+let numero = 0;
 function ChangeSlide(sense) {
 	numero = numero + sense;
 	if (numero > 3)
@@ -10,43 +28,29 @@ function ChangeSlide(sense) {
 	{
 		numero = 3;
 	}
-	document.getElementById("banner1").src = "./assets/images/slideshow/" + slides[numero];
+	for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('dot_selected');
+    }
+    dots[numero].classList.add('dot_selected');
+	
+	document.getElementById("banner1").src = "./assets/images/slideshow/" + slides[numero].image;
+	document.getElementById("tagLine").innerHTML = slides[numero].tagLine;
 
 	
 }
-const dots = ["slide1.jpg", "slide2.jpg", "slide4.png","slide3.jpg"]
 
+const dots = document.querySelectorAll('.dot'); 
 
-function Changedots(numero2) {
-	document.getElementById("banner1").src = "./assets/images/slideshow/" + dots[numero2];
+function Changedots(numero) {
+    
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('dot_selected');
+    }
+    dots[numero].classList.add('dot_selected');
 	
-}
-function Changeclass(numero2) {
-	document.getElementById(numero2).style.backgroundColor = '#fff';
-	
-}
+	document.getElementById("banner1").src = "./assets/images/slideshow/" + slides[numero].image;
+	document.getElementById("tagLine").innerHTML = slides[numero].tagLine;
 
-
-
-function ouvrirPage () {
-	var a = document.getElementById("#search").value;
 }
 
-// const slides = [
-// 	{
-// 		"image":"slide1.jpg",
-// 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-// 	},
-// 	{
-// 		"image":"slide2.jpg",
-// 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-// 	},
-// 	{
-// 		"image":"slide3.jpg",
-// 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-// 	},
-// 	{
-// 		"image":"slide4.png",
-// 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-// 	}
-// ]
+
